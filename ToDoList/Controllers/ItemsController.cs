dec6ac5d40f9154f49748d8c5b43ffbab88830b1
Svea Wade <svealinneawade@gmail.com>
2021-01-03 15:07:42 -0800

    updated the Items Controller in order to change the view to represent new DB

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,26 +1,25 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-//     private readonly ToDoListContext _db;
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     public ItemsController(ToDoListContext db)
-//     {
-//       _db = db;
-//     }
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Item> model = _db.Items.Include(items => items.Category).ToList();
-//       return View(model);
-//     }
+    public ActionResult Index()
+    {
+      return View(_db.Items.ToList());
+    }
 
 //     public ActionResult Create()
 //     {
@@ -71,5 +70,5 @@
 //       _db.SaveChanges();
 //       return RedirectToAction("Index");
 //     }
-//   }
-// }
+  }
+}
