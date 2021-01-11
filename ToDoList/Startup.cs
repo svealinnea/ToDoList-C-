2d8abd229655c4b8a2888ca71f40ddfd8d63bbd7
Svea Wade <svealinneawade@gmail.com>
2021-01-11 13:42:03 -0800

    adds personal to do not categories yet
    Co-authored-by: Garrett Dean Brown <gman9mm@live.com>

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoList.Models;
-using Microsoft.AspNetCore.Identity;
 
 namespace ToDoList
 {
@@ -28,10 +28,20 @@ namespace ToDoList
       services.AddEntityFrameworkMySql()
         .AddDbContext<ToDoListContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
-      
+
       services.AddIdentity<ApplicationUser, IdentityRole>()
-              .AddEntityFrameworkStores<ToDoListContext>()
-              .AddDefaultTokenProviders();
+        .AddEntityFrameworkStores<ToDoListContext>()
+        .AddDefaultTokenProviders();
+
+      services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
     }
 
     public void Configure(IApplicationBuilder app)
